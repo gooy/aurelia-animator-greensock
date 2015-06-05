bec9d9a3f917140bda81e1545a5d0e1ede552419
Aaike Van Roekeghem <aaikevr@gmail.com>
2015-06-06 04:08:09 +0700

    Update 2015-06-05T21:07:58.966Z

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import {Animator} from 'aurelia-templating';
-import {GreensockAnimator} from './animator';
-export {GreensockAnimator} from './animator';
-
-export function configure(aurelia, cb){
-  var animator = aurelia.container.get(GreensockAnimator);
-  Animator.configureDefault(aurelia.container, animator);
-  if(cb !== undefined && typeof(cb) === 'function') cb(animator);
-}
